dbac9677ffc7dea4e7e99557951b171ce2487ff0
Mwita Cyrus Nkorogo <84593129+mwitacyrus@users.noreply.github.com>
2023-05-06 14:08:37 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,36 +1,35 @@
 <?php
-  
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'mwitacyrus566@gmail.com';
+$receiving_email_address = 'mwitacyrus566@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+    include($php_email_form);
+} else {
+    die('Unable to load the "PHP Email Form" Library!');
+}
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+$contact->to = $receiving_email_address;
+$contact->from_name = $_POST['name'];
+$contact->from_email = $_POST['email'];
+$contact->subject = $_POST['subject'];
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+$contact->add_message($_POST['name'], 'From');
+$contact->add_message($_POST['email'], 'Email');
+$contact->add_message($_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+// Set headers
+$headers = "From: {$contact->from_name} <{$contact->from_email}>\r\n";
+$headers .= "Reply-To: {$contact->from_email}\r\n";
+$headers .= "Content-type: text/html\r\n";
+
+// Send the email using the PHP mail() function
+if (mail($contact->to, $contact->subject, $contact->message, $headers)) {
+    echo 'success';
+} else {
+    echo 'failed';
+}
+
 ?>
